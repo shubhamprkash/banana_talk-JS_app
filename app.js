b938a01e0b560cb148748d7e10b28caf9d8ddb68
ShubhamPrakash <56962038+shubhamprkash@users.noreply.github.com>
2023-01-02 00:19:04 +0530

    build init

---
+++ app.js
@@ -0,0 +1,8 @@
+// console.log("testing log on exrternal file")
+let btnTranslate = document.querySelector('#btn-translate');
+
+btnTranslate.addEventListener("click", triggerOne);
+
+function triggerOne(){
+    console.log("clicked")
+}
